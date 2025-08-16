b9c623b92428331ada7c2a78234301a9c8a17361
KneekoCo <Nsorensen@KneekoCo.com>
2025-08-16 15:14:34 -0600

    2

---
+++ packages/lib/cfr.ts
@@ -0,0 +1,11 @@
+﻿export const CFR = {
+  DAILY_22921: { intervalDays: 1 },
+  PERIODIC_22923: { intervalDays: 92 },
+  PERIODIC_22923_MICROMON: { intervalDays: 184 },
+  QMI_DAILY_33: { intervalDays: 33 },
+  ANNUAL_22927: { intervalDays: 368 },
+  AIR_L1_22929: { intervalDays: 368 },
+  AIR_L2_22929: { intervalDays: 368 },
+  AIR_L3_22929: { intervalDays: 736 },
+  RESERVOIR_22931: { intervalDays: 736 },
+};
